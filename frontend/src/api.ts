81e8a5d0041de4337e458587ffd54897e20e3921
chaitanyakhandare <chaitanyark.03@gmail.com@gmail.com>
2025-10-29 23:23:23 +0530

    backend deployed url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/weather", 
+  baseURL: "https://weather-insights-dolw.onrender.com/api/weather", 
+//   baseURL: "http://127.0.0.1:8000/api/weather", 
 });
 
 export default API;
